9c80c686fc6b181c5c0c27969411c52c7a831654
Anushal <anushal0483@gmail.com>
2022-05-24 18:33:17 +0530

    Moved to Folders

---
+++ src/Pages/ShowByPerson.js
@@ -0,0 +1,50 @@
+import React from "react";
+import '../Css/ShowByPerson.css';
+function ShowByPerson(props){
+
+
+    const onDelete=(e)=>{
+        e.preventDefault();
+        props.setShowPersonDetail(false);
+        var myHeaders = new Headers();
+        myHeaders.append("bearer",localStorage.getItem('bearer'));
+        myHeaders.append("user-id", localStorage.getItem('user-id'));
+  
+        var requestOptions = {
+        method: 'DELETE',
+        headers: myHeaders,
+        redirect: 'follow'
+        };
+  
+        fetch(`https://money-track-project.herokuapp.com//transactions/transactions/${e.target.value}`, requestOptions)
+        .then(response => response.json())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+      }
+
+return(
+    <div>
+       
+    <div className='popup'>
+    
+    <div className="price_table">
+  
+
+
+  <div className="package package_free"  >
+  <div className="boom" onClick={()=>props.setShowPersonDetail(false)}>close</div>
+    <h2 className='h2'>{props.Personal2.name}</h2>
+    <div className="price">    <div className="big"> &#8377; {props.Personal.amount}</div></div>
+    
+      <li className="list" style={{fontSize:"18px",fontWeight:"bold"}}>Last Date : {props.Personal.last_date}</li>
+      <li className="list" style={{fontSize:"18px",fontWeight:"bold"}}>Note : {props.Personal.note}</li>
+    
+    <button className='deleteButton' value={props.Personal.idencode} onClick={onDelete} >Delete</button>
+  </div>
+  
+  </div>
+</div> 
+</div>
+)
+}
+export default ShowByPerson;
---
+++ src/Pages/signIn.js
@@ -0,0 +1,91 @@
+import React,{ useState} from 'react';
+import axios from 'axios';
+import '../Css/SignIn.css';
+import logo from '../images/img.svg'
+import { Link,Navigate } from 'react-router-dom';
+function SignIn(){
+    const[loginInput,setLoginInput]=useState({
+        password: "",
+        username: "",
+        });
+
+    const handleChange=(e)=>{
+            setLoginInput({
+                ...loginInput,[e.target.name]:e.target.value
+        });
+    }
+
+    const[responseData,setResponseData]=useState({})
+    const[errorResponseData,setErrorResponseData]=useState({})
+    const[loading,setLoading]=useState("")
+
+    
+    const handleSubmit=(e)=>{
+        setLoading("Loading...");
+        e.preventDefault();
+        const payload = {
+            username: loginInput.username,
+            password: loginInput.password
+          }
+          
+          axios({
+            method: 'post',
+            url: 'https://money-track-project.herokuapp.com/accounts/login/',
+            data: payload,
+            headers: {
+             // 'Authorization': `bearer ${token}`,
+            'Content-Type': 'application/json'
+            }, 
+          }).then((response) => {
+            // console.log(response);
+            localStorage.setItem('bearer',response.data.bearer);
+            localStorage.setItem('user-id',response.data.idencode);
+            setResponseData(response.data);
+            setLoading("");
+            }
+            )
+        .catch((error) => {console.log('error', error)
+        setErrorResponseData(error.response.data);
+        setLoading("");
+          })
+    }
+    
+if(responseData.bearer){
+  return <div>
+    <Navigate to='/dashboard'/>
+    </div>
+}
+return (
+<div className='loginbody'>
+ 
+ <section className="loginside">
+        <img src={logo} alt=""/>
+    </section>
+
+    <section className="loginmain">
+        <div className="login-container">
+            <p className="logintitle">Welcome</p>
+            <div className="separator"></div>
+            <p className="welcome-message">Please, provide login credential to proceed and have access to all our services</p>
+
+            <form className="login-form">
+                <div className="loginform-control">
+                <input className='login-input' type="text"  placeholder="Username" name="username" value={loginInput.username} onChange={handleChange} required />
+                    <i className="fas fa-user"></i>
+                    <span  style={{color:"red"}}>{errorResponseData["username"]}</span> 
+                </div>
+                <div className="loginform-control">
+                <input className='login-input' type="password" name="password" placeholder="Password" value={loginInput.password} onChange={handleChange} required/>
+                    <i className="fas fa-lock"></i>
+                    <span style={{color:"red"}}>{errorResponseData["password"]}</span> 
+                </div>
+                <span style={{color:"red"}}>{errorResponseData.detail}</span>
+                <button onClick={handleSubmit} className="submit">{loading?loading:"Login"}</button>
+            </form>
+		<p>Don't have an Account? <Link to='/signUp'>Sign Up</Link></p>
+        </div>
+    </section>
+  </div>
+  );
+}
+export default SignIn;
