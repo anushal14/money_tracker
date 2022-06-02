aac6ffb59cb44c9d553795411525418c480919ce
Anushal <anushal0483@gmail.com>
2022-06-02 18:53:01 +0530

    Created new Child transaction page

--- src/Pages/ShowByPerson.js
+++ src/Pages/ShowByPerson.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React,{useState} from "react";
 import '../Css/ShowByPerson.css';
 import userImg from '../images/user.png'
+import AddChildTransaction from "./AddChildTransaction";
 function ShowByPerson(props){
 
-
+  const[newChildTransaction,setNewChildTransaction]=useState(false);
     const onDelete=(e)=>{
         e.preventDefault();
         props.setShowPersonDetail(false);
@@ -32,7 +33,7 @@ return(
 <span className='right'onClick={()=>props.setShowPersonDetail(false)}>cancel</span>
 </div>
 <div className="Child_btn">
-<button className='newButton'>New Transaction</button>
+<button className='newButton' onClick={()=>setNewChildTransaction(true)}>New Transaction</button>
 <button className='deleteButton' value={props.Personal.idencode} onClick={onDelete}>Delete</button>
 <span className="forTotal">Total Amount: <span style={{color:props.Personal.type===200?"red":"green"}}>&#8377; {props.Personal.amount}</span></span>
 </div>
@@ -102,6 +103,7 @@ return(
 </div>
 
 </div>
+{newChildTransaction && <AddChildTransaction setNewChildTransaction={setNewChildTransaction} personal2={props.Personal2.name} personal={props.Personal.type}/>}
 </div>
 )
 }
